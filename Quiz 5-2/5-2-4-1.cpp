c78800ecae7d7ec9fdb15f2d358811e9d3b5e005
Minh Anh Nguyen <tinawuyen3103@icloud.com>
2022-03-22 22:18:23 -0700

    Adding command

--- Quiz 5-2/5-2-4-1.cpp
+++ Quiz 5-2/5-2-4-1.cpp
@@ -4,3 +4,14 @@ using namespace std;
 
 int main()
 {
+ofstream ofs;
+    int Employee_ID;
+    string Employee_Name;
+    string Department_name;
+    double salary;
+
+    ofs.open("employee.txt");
+    if(!ofs){
+        cerr <<"File open Error \n";
+        exit(0);
+    } 
