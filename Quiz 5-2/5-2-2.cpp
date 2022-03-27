e352f899b9bff80285aeadfdf80d0c968a9f3f68
Kyuwoong Lee <kwooler@gmail.com>
2022-03-27 12:37:25 -0700

    5-2

--- Quiz 5-2/5-2-2.cpp
+++ Quiz 5-2/5-2-2.cpp
@@ -3,14 +3,19 @@ using namespace std;
 
 int main()
 {
-    int N, M;
-
+    int N, M, i;
+    int pow;
     cout << "Enter you input:";
     cin >> N;
-    M =1;
+    // M =1;
+    cin >> M; 
+    pow = 1;
+    i = 1;
     while (M <=N){
-        cout << M << "\t"<< N* M << endl;
-        M +=1;
+        // cout << M << "\t"<< N* M << endl;
+        // Instead of printing values, you should calculate the power value
+        pow *= M; // Let's assume M = 2 N = 4, pow will be 2, 4, 8, 16
+        i +=1;
     }
 
 }
