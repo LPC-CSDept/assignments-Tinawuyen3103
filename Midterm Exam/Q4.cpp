653dd1618b572dc801b2715f99cf1ea5f5123521
Minh Anh Nguyen <97919286+Tinawuyen3103@users.noreply.github.com>
2022-04-17 16:22:05 -0700

    Correction

--- Midterm Exam/Q4.cpp
+++ Midterm Exam/Q4.cpp
@@ -7,7 +7,7 @@ int getRdnum(){
 }
 int getRdnum(int n1){
    srand ( time(NULL) );
-   return rand() % (101-n1)+n1;
+   return rand() % (101-n1);
 }
 int getRdnum(int n1,int n2){
    srand ( time(NULL) );
