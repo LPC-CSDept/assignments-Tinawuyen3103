d9ec6165537c3c7fdf104c9b9761983030bf0205
Minh Anh Nguyen <tinawuyen3103@icloud.com>
2022-04-17 15:31:55 -0700

    Complete

---
+++ Midterm Exam/Q3.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int precedingNum; 
+
+int getRdnum() {
+	return 1 + (rand() % 50);
+}
+
+int isGreater(int n) {
+	if (n > precedingNum) {
+		return 1;
+	}
+
+	return 0;
+}
+
+int main()
+{
+	srand(time(nullptr));
+
+	ofstream ofs("numbers.txt"); 
+	if (!ofs.is_open()) { 
+		cout << "Can not open numbers.txt for writing" << endl;
+		return 1; 
+	}
+
+	int N = 10;
+	int ranNum = getRdnum();
+	ofs << ranNum; 
+	precedingNum = ranNum; 
+	N--;
+
+	while (N > 0) { 
+		ranNum = getRdnum();
+		if (isGreater(ranNum)) { 
+			ofs << " " << ranNum; 
+			precedingNum = ranNum; 
+		}
+		N--;
+	}
+
+	ofs.close(); 
+
+	return 0;
+}
