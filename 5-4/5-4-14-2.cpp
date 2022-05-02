6dce1484e42ee9264f9ff885dde4b4bd301177c9
Minh Anh Nguyen <97919286+Tinawuyen3103@users.noreply.github.com>
2022-05-01 23:48:53 -0700

    Update 5-4-14-2.cpp

--- 5-4/5-4-14-2.cpp
+++ 5-4/5-4-14-2.cpp
@@ -6,37 +6,37 @@
 
 using namespace std;
 int main () {
-    int N, i;
-    double sum = 0, average;
-    cout << "How many random numbers would you like to generate?" << endl;
-    cin >> N;
-    int a[N];
-    
-    srand(time(0));
-    ofstream file ("testfile.txt");
-
-    for (i = 0; i < N; i++)
-    {
-         
-        a[i] = (rand() % 100) + 1;
-        file << a[i] << "\n";
-        sum = sum + a[i];
-	    
-	    if (a[i] < min) {
-		    min = a[i];
-	    }
-	    if (a[i] > max) {
-		    max = a[i];
-	    }
-    }
-    file.close();
-    
-    average = sum/N;
-    
-    cout << "Total of integers: " << N << endl;
-    cout << "The sum is: " << sum << endl;
-    cout << "The min is : " << min << endl;
-    cout << "The max is: " << max << endl;
+	int N, i;
+	double sum = 0, average;
+	cout << "How many random numbers would you like to generate?" << endl;
+	cin >> N;
+	int a[N];
+	int min, max;
+	
+	srand(time(0));
+	ofstream file ("testfile.txt");
+	
+	for (i = 0; i < N; i++)
+	{
+		a[i] = (rand() % 100) + 1;
+		file << a[i] << "\n";
+		sum = sum + a[i];
+		
+		if (a[i] < min) {
+			min = a[i];
+		}
+		if (a[i] > max) {
+			max = a[i];
+		}
+	}
+	file.close();
+	
+	average = sum/N;
+	
+	cout << "Total of integers: " << N << endl;
+	cout << "The sum is: " << sum << endl;
+	cout << "The min is : " << min << endl;
+	cout << "The max is: " << max << endl;
 	cout << "The average between all random numbers is: " << average << endl;
 	
 	return 0;
